9f1a35e77f5c0fcadb7b5614df72aab092ad27be
JhonAlisHarol <aharolj@gmail.com>
2026-06-21 13:47:15 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import unicodedata
 USUARIOS_AUTORIZADOS = {
     "CON-C5": "12345", "CORCOL": "CORCOLONC3", "COMCH": "12345678",
     "COMAR": "hex123", "COMDA": "hex123", "COMCHEP": "hex123",
-    "CEVIBO": "hex123", "COMSAM": "hex123", "ELMER RODRIGUEZ": "ELIZAharol31", "MAYOR LORENZO BOWEN": "10924"
+    "CEVIBO": "hex123", "COMSAM": "hex123", "ALISJHON": "199432", "LORENZO BOWEN": "10924"
 }
 
 def login():
